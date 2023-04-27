e08619ab3129e4f25e589aa51b8d494f0597c1dd
Okechukwu Joshua <lodianaselora@gmail.com>
2023-04-27 19:21:03 +0100

    Update urls.py

--- Api/urls.py
+++ Api/urls.py
@@ -1,8 +1,8 @@
-from django.contrib import admin  
-from django.urls import path  
-from django.urls.conf import include  
-from django.conf import settings  
-from django.conf.urls.static import static  
+from django.contrib import admin
+from django.urls import path
+from django.urls.conf import include
+from django.conf import settings
+from django.conf.urls.static import static
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -18,5 +18,5 @@ urlpatterns = [
     path('', include('frontend.urls'))
 ]
 
-if settings.DEBUG:  
-        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT) 
+if settings.DEBUG:
+        urlpatterns += static(settings.MEDIA_URL,document_root=settings.MEDIA_ROOT)
